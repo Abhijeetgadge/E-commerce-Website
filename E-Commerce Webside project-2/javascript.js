76b0efc44ade3debad13037d7643fb199dcb0538
Abhijeetgadge <2015bec109@sggs.ac.in>
2024-04-30 22:45:28 +0530

    e-commers

---
+++ E-Commerce Webside project-2/javascript.js
@@ -0,0 +1,77 @@
+// console.log("This is the javascript");
+// let employee ={
+//     name : "Abhi",
+//     salary : 10,
+//     channel : "Abh",
+// }
+// console.log(employee)
+// // let names = [1,2,3, "Abhi" ,undefined];
+// let names = new Array(41,2,4, "loki", undefined);
+// names = names.sort();
+// names.push("this is Abhi");
+// console.log(names)
+// function greet(name){
+//     console.log(name + " is a good boy");
+// }
+// let name = "Abhi";
+// let name1 = "shubham";
+// let name2 = "Amol";
+// let name3 = "pama";
+// greet(name);
+// greet(name1);
+// greet(name2);
+// greet(name3);
+// function sum(a,b,c){
+//     let d = a + b + c;
+//     return d;
+// }
+// let returnVal = sum(3,3,3);
+// console.log(returnVal);
+// function great(a,b){
+//     if(a>b){
+//         console.log(a + " is greater than " + b);
+//     }
+//     else if(a==b){
+//         console.log(a + " is equal to " + b);
+//     }
+//     else{
+//         console.log(a + " is less than " + b);
+//     }
+// }
+// great(45,45);
+// Alert in in-browser JS 
+// alert("This is the alrt message");
+// prompt in js 
+// let age = prompt("What is your age", "18");
+// if(age>=18){
+//     console.log("we are allow to you enter the club");
+// }
+// else{
+//     console.log("Take permission from your parent to enter club");
+// }
+// console.log(age);
+// let deletepost = confirm("Do you really want to delete this post");
+// if(deletepost){
+//     console.log("your post has been deleted successfully");
+// }
+// else{
+//     console.log("your post has not been deleted")
+// }
+// for(let i = 0; i < friends.length; i++){
+    //     console.log("Hello friends, " + friends[i]);
+    // }
+    
+    // friends.forEach(function f(element){
+        //     console.log("Hello friend, " + element + " to modern JavaScript");
+        // })
+// let friends = ["Abhi","Rahul","pama","kartik"];
+// for(element of friends){
+//     console.log("Hello fiends, " + element + " to modern js again")
+// }
+let i = 0;
+while(){
+    console.log('${i} i is less than 4
+}
+
+
+
